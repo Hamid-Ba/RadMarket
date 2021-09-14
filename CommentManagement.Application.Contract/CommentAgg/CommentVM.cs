b631ff51e03ddaf6e652cf827689b4bc93b0e63e
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-09-14 11:54:55 +0430

    Add Comment Application Contract

---
+++ CommentManagement.Application.Contract/CommentAgg/CommentVM.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Framework.Application;
+
+namespace CommentManagement.Application.Contract.CommentAgg
+{
+    public class CommentVM
+    {
+        public long Id { get; set; }
+        public long StoreId { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Message { get; set; }
+        public bool IsConfirmed { get; set; }
+        public int Type { get; set; }
+        public long OwnerId { get; set; }
+        public string OwnerName { get; set; }
+
+        public long ParentId { get; set; }
+    }
+
+    public class CreateCommentVM
+    {
+        public long OwnerId { get; set; }
+        public string OwnerName { get; set; }
+        public int Type { get; set; }
+        public long ParentId { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        [EmailAddress(ErrorMessage = "ایمیل نامعتبر هست !")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string Message { get; set; }
+    }
+
+    public class SearchCommentVM
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ CommentManagement.Application.Contract/CommentAgg/ICommentApplication.cs
@@ -0,0 +1,12 @@
+﻿using Framework.Application;
+
+namespace CommentManagement.Application.Contract.CommentAgg
+{
+    public interface ICommentApplication
+    {
+        OperationResult Delete(long id);
+        OperationResult Confirm(long id);
+        OperationResult DisConfirm(long id);
+        OperationResult Create(CreateCommentVM command);
+    }
+}
