e283ab9e7b9e6d3208ec85abf4ab554b0db35482
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-09-14 10:30:41 +0430

    Add Discount Management Application Contracts

---
+++ DiscountManagement.Application.Contract/DiscountAgg/DiscountVM.cs
@@ -0,0 +1,53 @@
+﻿using Framework.Application;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DiscountManagement.Application.Contract.DiscountAgg
+{
+    public class DiscountVM
+    {
+        public long Id { get; set; }
+        public long StoreId { get;  set; }
+        public long ProductId { get;  set; }
+        public int DiscountRate { get;  set; }
+        public DateTime StartDate { get;  set; }
+        public DateTime EndDate { get;  set; }
+        public string Reason { get;  set; }
+    }
+
+    public class CreateDiscountVM
+    {
+        [Display(Name = "فروشگاه")]
+        public long StoreId { get; set; }
+
+        [Display(Name = "محصول")]
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        [Range(1, long.MaxValue, ErrorMessage = ValidationMessage.IsRequired)]
+        public long ProductId { get; set; }
+
+        [Display(Name = "درصد تخفیف")]
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        [Range(1, 100, ErrorMessage = ValidationMessage.IsRequired)]
+        public int DiscountRate { get; set; }
+
+        [Display(Name = "تاریخ شروع")]
+        [MinLength(10,ErrorMessage = "حداقل کاراکتر مجاز {1} می باشد")]
+        [MaxLength(10,ErrorMessage = "حداقل کاراکتر مجاز {1} می باشد")]
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string StartDate { get; set; }
+
+        [Display(Name = "تاریخ پایان")]
+        [MinLength(10, ErrorMessage = "حداقل کاراکتر مجاز {1} می باشد")]
+        [MaxLength(10, ErrorMessage = "حداقل کاراکتر مجاز {1} می باشد")]
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string EndDate { get; set; }
+
+        [Display(Name = "دلیل")]
+        public string Reason { get; set; }
+    }
+
+    public class EditDiscountVM  : CreateDiscountVM
+    {
+        public long Id { get; set; }
+    }
+}
---
+++ DiscountManagement.Application.Contract/DiscountAgg/IDiscountApplication.cs
@@ -0,0 +1,13 @@
+﻿using Framework.Application;
+using System.Threading.Tasks;
+
+namespace DiscountManagement.Application.Contract.DiscountAgg
+{
+    public interface IDiscountApplication
+    {
+        Task<OperationResult> Delete(long id);
+        Task<EditDiscountVM> GetDetailForEditBy(long id);
+        Task<OperationResult> Edit(EditDiscountVM command);
+        Task<OperationResult> Create(CreateDiscountVM command);
+    }
+}
