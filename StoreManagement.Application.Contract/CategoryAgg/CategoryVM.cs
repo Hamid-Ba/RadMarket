b62369d43c0a66393f28605403840a9d7dc51f3a
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-09-16 11:20:45 +0430

    Add Category Application Contract

---
+++ StoreManagement.Application.Contract/CategoryAgg/CategoryVM.cs
@@ -0,0 +1,41 @@
+﻿using Framework.Application;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreManagement.Application.Contract.CategoryAgg
+{
+    public class CategoryVM
+    {
+        public long Id { get;  set; }
+        public string Name { get;  set; }
+        public string Description { get;  set; }
+        public string KeyWords { get;  set; }
+        public string MetaDescription { get;  set; }
+        public string Slug { get;  set; }
+    }
+
+    public class CreateCategoryVM
+    {
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string Name { get; set; }
+
+        [Display(Name = "توضیحات")]
+        public string Description { get; set; }
+
+        [Display(Name = "کلمات کلیدی")]
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string KeyWords { get; set; }
+
+        [Display(Name = "توضیحات متا")]
+        public string MetaDescription { get; set; }
+
+        [Display(Name = "اسلاگ")]
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string Slug { get; set; }
+    }
+
+    public class EditCategoryVM : CreateCategoryVM
+    {
+        public long Id { get; set; }
+    }
+}
---
+++ StoreManagement.Application.Contract/CategoryAgg/ICategoryApplication.cs
@@ -0,0 +1,13 @@
+﻿using Framework.Application;
+using System.Threading.Tasks;
+
+namespace StoreManagement.Application.Contract.CategoryAgg
+{
+    public interface ICategoryApplication
+    {
+        Task<OperationResult> Delete(long id);
+        Task<EditCategoryVM> GetDetailForEditBy(long id);
+        Task<OperationResult> Edit(EditCategoryVM command);
+        Task<OperationResult> Create(CreateCategoryVM command);
+    }
+}
