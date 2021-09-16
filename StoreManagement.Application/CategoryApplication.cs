33ac06a9266001bcbfda974e6e7dbe1bc1bfa11c
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-09-16 11:29:47 +0430

    Add Category Application

---
+++ StoreManagement.Application/CategoryApplication.cs
@@ -0,0 +1,61 @@
+﻿using Framework.Application;
+using StoreManagement.Application.Contract.CategoryAgg;
+using StoreManagement.Domain.CategoryAgg;
+using System.Threading.Tasks;
+
+namespace StoreManagement.Application
+{
+    public class CategoryApplication : ICategoryApplication
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryApplication(ICategoryRepository categoryRepository) => _categoryRepository = categoryRepository;
+
+        public async Task<OperationResult> Create(CreateCategoryVM command)
+        {
+            OperationResult result = new();
+
+            if (_categoryRepository.Exists(c => c.Name == command.Name || c.Slug == command.Slug))
+                return result.Failed(ApplicationMessage.DuplicatedModel);
+
+            var category = new Category(command.Name, command.Description, command.KeyWords, command.MetaDescription, command.Slug.Slugify());
+
+            await _categoryRepository.AddEntityAsync(category);
+            await _categoryRepository.SaveChangesAsync();
+
+            return result.Succeeded();
+        }
+
+        public async Task<OperationResult> Delete(long id)
+        {
+            OperationResult result = new();
+
+            var category = await _categoryRepository.GetEntityByIdAsync(id);
+            if (category is null) return result.Failed(ApplicationMessage.NotExist);
+
+            category.Delete();
+            await _categoryRepository.SaveChangesAsync();
+
+            return result.Succeeded();
+        }
+
+        public async Task<OperationResult> Edit(EditCategoryVM command)
+        {
+            OperationResult result = new();
+
+            var category = await _categoryRepository.GetEntityByIdAsync(command.Id);
+
+            if (category is null) return result.Failed(ApplicationMessage.NotExist);
+            if (_categoryRepository.Exists(c => (c.Name == command.Name || c.Slug == command.Slug) && c.Id != command.Id))
+                return result.Failed(ApplicationMessage.DuplicatedModel);
+
+            category.Edit(command.Name, command.Description, command.KeyWords, command.MetaDescription, command.Slug.Slugify());
+            await _categoryRepository.SaveChangesAsync();
+
+            return result.Succeeded();
+        }
+
+        public async Task<EditCategoryVM> GetDetailForEditBy(long id) => await _categoryRepository.GetDetailForEditBy(id);
+        
+    }
+}
--- StoreManagement.Domain/CategoryAgg/ICategoryRepository.cs
+++ StoreManagement.Domain/CategoryAgg/ICategoryRepository.cs
@@ -1,8 +1,11 @@
 ﻿using Framework.Domain;
+using StoreManagement.Application.Contract.CategoryAgg;
+using System.Threading.Tasks;
 
 namespace StoreManagement.Domain.CategoryAgg
 {
     public interface ICategoryRepository : IRepository<Category>
     {
+        Task<EditCategoryVM> GetDetailForEditBy(long id);
     }
 }
