feb7359bc29a4231787facecae181e7f43585706
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-09-18 11:09:32 +0430

    Initial DataBase For Adding Product Model

---
+++ StoreManagement.Infrastructure.EfCore/Migrations/20210918060515_CreateProductEntity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StoreManagement.Infrastructure.EfCore.Migrations
+{
+    public partial class CreateProductEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StoreId = table.Column<long>(type: "bigint", nullable: false),
+                    CategoryId = table.Column<long>(type: "bigint", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Picture = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    PictureAlt = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    PictureTitle = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    EachBoxCount = table.Column<int>(type: "int", nullable: false),
+                    ConsumerPrice = table.Column<double>(type: "float", nullable: false),
+                    PurchacePrice = table.Column<double>(type: "float", nullable: false),
+                    MoneyGain = table.Column<double>(type: "float", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    Prize = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Slug = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: false),
+                    Keywords = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    MetaDescription = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    ProductAcceptOrRejectDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductAcceptanceState = table.Column<int>(type: "int", maxLength: 250, nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeletionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Products_Stores_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "Stores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_StoreId",
+                table: "Products",
+                column: "StoreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
