5b7f3f797b37b949ddedaa530c22566f97fb9148
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-09-22 12:14:08 +0330

    Add Store Area

---
+++ ServiceHost/Areas/Store/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Areas.Store.Controllers
+{
+    public class DashboardController : StoreBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ServiceHost/Areas/Store/Controllers/StoreBaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Areas.Store.Controllers
+{
+    [Area("Store")]
+    public class StoreBaseController : Controller
+    {
+        protected string ErrorMessage = "ErrorMessage";
+        protected string SuccessMessage = "SuccessMessage";
+        protected string InfoMessage = "InfoMessage";
+        protected string WarningMessage = "WarningMessage";
+    }
+}
