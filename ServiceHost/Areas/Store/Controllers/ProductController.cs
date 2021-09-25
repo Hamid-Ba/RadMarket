08d9b00f0e96f2b3a1fde95e58e230eff71b5cca
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-09-25 12:15:43 +0330

    Complete Pagination In Product Index Store Panel

--- ServiceHost/Areas/Store/Controllers/ProductController.cs
+++ ServiceHost/Areas/Store/Controllers/ProductController.cs
@@ -26,7 +26,7 @@ namespace ServiceHost.Areas.Store.Controllers
 
             var model = PagingList.Create(products, 10, pageIndex);
 
-            ViewBag.Rows = (10 * pageIndex) - 9;
+            ViewBag.Rows = (10 * pageIndex);
 
             model.RouteValue = new RouteValueDictionary()
             {
