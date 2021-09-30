a2013c73fb2b8b059f3cbda2d310f2b55fa98eca
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-09-30 06:12:13 +0330

    Complete Products List Page

--- ServiceHost/Controllers/ProductsController.cs
+++ ServiceHost/Controllers/ProductsController.cs
@@ -1,53 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using RadMarket.Query.Contracts.CategoryAgg;
 using RadMarket.Query.Contracts.ProductAgg;
 using ReflectionIT.Mvc.Paging;
 using System.Threading.Tasks;
 
 namespace ServiceHost.Controllers
 {
-    public class CategoriesController : Controller
+    public class ProductsController : Controller
     {
         private readonly IProductQuery _productQuery;
-        private readonly ICategoryQuery _categoryQuery;
 
-        public CategoriesController(ICategoryQuery categoryQuery, IProductQuery productQuery)
-        {
-            _productQuery = productQuery;
-            _categoryQuery = categoryQuery;
-        }
+        public ProductsController(IProductQuery productQuery) => _productQuery = productQuery;
 
-        public async Task<IActionResult> Index() => View(await _categoryQuery.GetAll());
-
-        [Route("Incrideble-Offers")]
+        [HttpGet("Incrideble-Offers")]
         public async Task<IActionResult> IncridebleOffers(int pageIndex = 1)
         {
-            var products= await _productQuery.GetAll("Discount");
+            var products = await _productQuery.GetAll("Discount");
 
             var model = PagingList.Create(products, 6, pageIndex);
+            model.Action = "IncridebleOffers";
 
             return View(model);
         }
 
-        [Route("Best-Offers")]
+        [HttpGet("Best-Offers")]
         public async Task<IActionResult> BestOffers(int pageIndex = 1)
         {
             var products = await _productQuery.GetAll("BestSells");
 
             var model = PagingList.Create(products, 6, pageIndex);
+            model.Action = "BestOffers";
 
             return View(model);
         }
 
-        [Route("Products")]
+        
+        [HttpGet("Products")]
+        [HttpGet]
         public async Task<IActionResult> Products(int pageIndex = 1)
         {
             var products = await _productQuery.GetAll("Routine");
 
-            var model = PagingList.Create(products, 2, pageIndex);
+            var model = PagingList.Create(products, 6, pageIndex);
+            model.Action = "Products";
 
             return View(model);
         }
-
     }
 }
