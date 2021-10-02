69c41c9185232fa31ae6b8fdf23d69a2cc6b0143
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-10-02 09:08:21 +0330

    Complete Package Admin Front

---
+++ ServiceHost/Areas/Admin/Controllers/PackageController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreManagement.Application.Contract.PackageAgg;
+using System.Threading.Tasks;
+
+namespace ServiceHost.Areas.Admin.Controllers
+{
+    public class PackageController : AdminBaseController
+    {
+        private readonly IPackageApplication _packageApplication;
+
+        public PackageController(IPackageApplication packageApplication) => _packageApplication = packageApplication;
+
+        public async Task<IActionResult> Index() => View(await _packageApplication.GetAll());
+
+        [HttpGet]
+        public IActionResult Create() => PartialView();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreatePackageVM command)
+        {
+            var result = await _packageApplication.Create(command);
+
+            if (result.IsSucceeded) TempData[SuccessMessage] = result.Message;
+
+            return new JsonResult(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(long id) => PartialView(await _packageApplication.GetDetailForEditBy(id));
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EditPackageVM command)
+        {
+            var result = await _packageApplication.Edit(command);
+
+            if (result.IsSucceeded) TempData[SuccessMessage] = result.Message;
+
+            return new JsonResult(result);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(long id) => PartialView(id);
+
+        [ActionName("Delete")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> PostDelete(long id)
+        {
+            var result = await _packageApplication.Delete(id);
+
+            if (result.IsSucceeded) TempData[SuccessMessage] = result.Message;
+
+            return new JsonResult(result);
+        }
+    }
+}
