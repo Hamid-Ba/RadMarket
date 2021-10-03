97d6fa7a2c7b1a622ad4fac320aab0a6b9cf47ca
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-10-03 18:54:55 +0330

    Add Ticket Application Contract Lib

---
+++ TicketManagement.Application.Contract/StoreTicketAgg/IStoreTicketApplication.cs
@@ -0,0 +1,11 @@
+﻿using Framework.Application;
+using System.Threading.Tasks;
+
+namespace TicketManagement.Application.Contract.StoreTicketAgg
+{
+    public interface IStoreTicketApplication
+    {
+        Task<OperationResult> Create(CreateStoreTicketVM command);
+        Task<OperationResult> SendMessage(AddStoreTicketMessageVM command);
+    }
+}
---
+++ TicketManagement.Application.Contract/StoreTicketAgg/StoreTicketVM.cs
@@ -0,0 +1,47 @@
+﻿using Framework.Application;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketManagement.Application.Contract.StoreTicketAgg
+{
+    public class StoreTicketVM
+    {
+        public long Id { get; set; }
+        public string Title { get;  set; }
+        public long FirstStoreId { get; set; }
+        public long SecondStoreId { get; set; }
+        public string CreationDate { get; set; }
+    }
+
+    public class CreateStoreTicketVM
+    {
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public long FirstStoreId { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        [Range(1,long.MaxValue,ErrorMessage = ValidationMessage.IsRequired)]
+        public long SecondStoreId { get; set; }
+
+        [Display(Name = "پیام")]
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string Message { get; set; }
+    }
+
+    public class AddStoreTicketMessageVM
+    {
+        public long StoreTicketId { get;  set; }
+        public long SenderId { get;  set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        [Range(1, long.MaxValue, ErrorMessage = ValidationMessage.IsRequired)]
+        public long ReciverId { get;  set; }
+
+        [Display(Name = "پیام")]
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string Message { get;  set; }
+    }
+}
