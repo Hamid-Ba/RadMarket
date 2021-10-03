ffdafd7f5379520bb7fc63bb2ff5b5a884157d65
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-10-03 13:39:42 +0330

    Complete Adt Package Store Front Section

---
+++ ServiceHost/Areas/Store/Controllers/AdtPackageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreManagement.Application.Contract.AdtPackageAgg;
+using System.Threading.Tasks;
+
+namespace ServiceHost.Areas.Store.Controllers
+{
+    public class AdtPackageController : StoreBaseController
+    {
+        private readonly IAdtPackageApplication _adtPackageApplication;
+
+        public AdtPackageController(IAdtPackageApplication adtPackageApplication) => _adtPackageApplication = adtPackageApplication;
+
+        public async Task<IActionResult> Index() => View(await _adtPackageApplication.GetAll());
+    }
+}
