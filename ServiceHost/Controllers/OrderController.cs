a319746a38e029926714d56628d93e09ece32c9f
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-10-10 11:13:34 +0330

    Add Order Controller And Add Add Product To Order Action

---
+++ ServiceHost/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreManagement.Application.Contract.OrderAgg;
+using System.Threading.Tasks;
+
+namespace ServiceHost.Controllers
+{
+    public class OrderController : BaseController
+    {
+        private readonly IOrderApplication _orderApplication;
+
+        public OrderController(IOrderApplication orderApplication)
+        {
+            _orderApplication = orderApplication;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AddProductToOpenOrder(AddOrderItemsVM command)
+        {
+            var result = await _orderApplication.AddProductToOpenOrder(command);
+
+            if (result.IsSucceeded)
+            {
+                TempData[SuccessMessage] = result.Message;
+                return View();
+            }
+
+            TempData[ErrorMessage] = result.Message;
+            return View();
+        }
+    }
+}
