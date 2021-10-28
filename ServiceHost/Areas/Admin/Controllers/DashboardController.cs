66b7f1676d58b735df3f40792ea6ca104403df2a
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-10-28 11:55:13 +0330

    The End

--- ServiceHost/Areas/Admin/Controllers/DashboardController.cs
+++ ServiceHost/Areas/Admin/Controllers/DashboardController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServiceHost.Tools;
 
 namespace ServiceHost.Areas.Admin.Controllers
 {
+    [AdminPermissionChecker(1)]
     public class DashboardController : AdminBaseController
     {
         public IActionResult Index()
