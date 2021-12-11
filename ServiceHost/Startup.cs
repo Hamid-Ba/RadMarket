f9d0380977de559c7f48d2def2ca74083a0c0a94
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-12-11 16:14:03 +0330

    Change Error development mode page

--- ServiceHost/Startup.cs
+++ ServiceHost/Startup.cs
@@ -83,7 +83,8 @@ namespace ServiceHost
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/ErrorOccurred");
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
